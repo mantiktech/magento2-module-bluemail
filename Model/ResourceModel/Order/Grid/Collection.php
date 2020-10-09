f2a5b78f4e1c7371e025a7a56775f9a1180a01d3
Tiberio Benvenuto <tiberiobenvenuto@gmail.com>
2020-10-09 09:12:15 -0300

    BM-massshipping

---
+++ Model/ResourceModel/Order/Grid/Collection.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Copyright ©  2020. Mantik Tech.  All rights reserved under CC BY-NC-SA 4.0 licence.
+ * See LICENSE file for more details.
+ * @link https://www.mantik.tech/
+ */
+
+namespace Mantik\Bluemail\Model\ResourceModel\Order\Grid;
+
+use Magento\Framework\Data\Collection\Db\FetchStrategyInterface as FetchStrategy;
+use Magento\Framework\Data\Collection\EntityFactoryInterface as EntityFactory;
+use Magento\Framework\Event\ManagerInterface as EventManager;
+use Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult;
+use Psr\Log\LoggerInterface as Logger;
+
+class Collection extends SearchResult
+{
+    /**
+     * Initialize dependencies.
+     *
+     * @param EntityFactory $entityFactory
+     * @param Logger        $logger
+     * @param FetchStrategy $fetchStrategy
+     * @param EventManager  $eventManager
+     * @param string        $mainTable
+     * @param string        $resourceModel
+     *
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function __construct(
+        EntityFactory $entityFactory,
+        Logger $logger,
+        FetchStrategy $fetchStrategy,
+        EventManager $eventManager,
+        $mainTable = 'sales_order_grid',
+        $resourceModel = \Magento\Sales\Model\ResourceModel\Order::class
+    ) {
+        parent::__construct($entityFactory, $logger, $fetchStrategy, $eventManager, $mainTable, $resourceModel);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function _initSelect()
+    {
+        parent::_initSelect();
+
+        $this
+            ->getSelect()
+            ->joinLeft(
+                ['sales_order' => $this->getTable('sales_order')],
+                'main_table.entity_id = sales_order.entity_id',
+                ['shipping_method' => 'sales_order.shipping_method']
+            );
+
+        $this
+            ->getSelect()
+            ->where('sales_order.shipping_method like "bluemail%"');
+
+        $tableDescription = $this->getConnection()->describeTable($this->getMainTable());
+        foreach ($tableDescription as $columnInfo) {
+            $this->addFilterToMap($columnInfo['COLUMN_NAME'], 'main_table.' . $columnInfo['COLUMN_NAME']);
+        }
+
+        return $this;
+    }
+}
